34037f7c52138449aca338bc85addf48ea9ad96e
Viktor Bartolic <viktor.bartolic@burza.hr>
2022-03-24 21:22:59 +0100

    add new icon

---
+++ components/Icons/BookmarkIcon.tsx
@@ -0,0 +1,17 @@
+export const BookmarkIcon = ({ height }: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    height={height}
+    viewBox="0 0 24 24"
+    stroke="#5e8dce"
+    strokeWidth={2}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+    />
+  </svg>
+);
